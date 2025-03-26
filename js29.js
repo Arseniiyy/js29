a5e5abfcb5ff0e57f1f082c061073d005a89c40c
Arsenii Penkovski <Penkovskiy2024@mail.ru>
2025-03-26 20:54:16 +0300

    first commit

---
+++ js29.js
@@ -0,0 +1,94 @@
+// const sum = (a, b) => a + b;
+
+
+// const isPositive = number => number >= 0;
+
+
+// const randomNumber = () => Math.random();
+
+
+// const multiply = (a, b) => a * b;
+
+
+// const greet = name => `Привет, ${name}!`;
+
+
+// const max = (a, b) => (a > b ? a : b);
+
+
+// const isEven = number => number % 2 === 0;
+
+
+// const reverseString = str => str.split('').reverse().join('');
+
+
+// const factorial = n => {
+//     if (n === 0) return 1;
+//     return n * factorial(n - 1);
+// };
+
+
+// const createCounter = () => {
+//     let count = 0;
+//     return {
+//         increment: () => ++count,
+//         decrement: () => --count
+//   };
+// };
+
+
+// const greet = function(name) {
+//     console.log(`Привет, ${name}!`);
+// };
+
+
+// greet('Алексей');  
+
+// const calculate = function(num1, num2, callback) {
+//     return callback(num1, num2);
+// };
+
+
+// const add = function(a, b) {
+//     return a + b;
+// };
+
+// const subtract = function(a, b) {
+//     return a - b;
+// };
+
+
+
+// const welcome = function() {
+//     console.log('Добро пожаловать!');
+// };
+
+
+
+// const createCalculator = function() {
+//     return {
+//         add: function(a, b) {
+//             return a + b;
+//         },
+//         subtract: function(a, b) {
+//             return a - b;
+//         },
+//         multiply: function(a, b) {
+//             return a * b;
+//         },
+//         divide: function(a, b) {
+//             if (b === 0) {
+//                 return 'Ошибка: Деление на ноль!';
+//             }
+//             return a / b;
+//         }
+//     };
+// };
+
+
+// const calculator = createCalculator();
+// console.log(calculator.add(10, 5));      
+// console.log(calculator.subtract(10, 5)); 
+// console.log(calculator.multiply(10, 5)); 
+// console.log(calculator.divide(10, 5));   
+// console.log(calculator.divide(10, 0));
